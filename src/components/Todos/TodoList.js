0f18fe79bfc4aad4f25a4785bb0aae85d45e2bc7
George Theos <g.theos@gmail.com>
2023-01-13 11:44:04 +0200

    custom hook and checkbox working, fix git

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -5,7 +5,13 @@ const TodoList = (props) => {
   return (
     <ul className={classes['todo-list']}>
       {props.todos.map((todo) => (
-        <TodoItem key={todo.id} id={todo.id} checked={todo.checked} onDelete={props.onDeleteItem} onCheck={props.onCheckItem}>
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          checked={todo.checked}
+          onDelete={props.onDeleteItem}
+          onCheck={props.onCheckItem}
+        >
           {todo.text}
         </TodoItem>
       ))}
